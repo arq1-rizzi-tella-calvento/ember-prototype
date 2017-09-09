a831976e9f7b8cee50e17c0b6e47717fd21777f1
patriciotella <patriciotella@gmail.com>
2017-09-09 13:54:01 -0300

    Se agregan comisiones para cada materia y se modifica estilo al prototipo

--- app/routes/materias.js
+++ app/routes/materias.js
@@ -3,10 +3,36 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return [
-      { name: 'Estructura de datos', class_schedule: '21-22hs', days: 'Lunes y miercoles' },
-      { name: 'Matematica II', class_schedule: '8-12hs', days: 'Viernes' },
-      { name: 'Ingles I', class_schedule: '14-16hs', days: 'Jueves' },
-      { name: 'Programación concurrente', class_schedule: '18-22hs', days: 'Martes' }
+      { name: 'Estructura de datos',
+        commission:[
+          { name: 'C1', class_schedule: '21-22hs', days: 'Lunes y miercoles'},
+          { name: 'C2', class_schedule: '21-22hs', days: 'Jueves y Viernes'}
+        ]
+      },
+      { name: 'Matematica II',
+        commission:[
+          { name: 'C1', class_schedule: '8-12hs', days: 'Viernes'},
+          { name: 'C2', class_schedule: '8-12hs', days: 'Lunes'}
+        ]
+      },
+      { name: 'Ingles I',
+        commission:[
+          { name: 'C1', class_schedule: '14-16hs', days: 'Jueves'},
+          { name: 'C2', class_schedule: '14-16hs', days: 'Martes'}
+        ]
+      },
+      { name: 'Programación concurrente',
+        commission:[
+          { name: 'C1', class_schedule: '18-22hs', days: 'Martes' },
+          { name: 'C2',  class_schedule: '18-22hs', days: 'Viernes' }
+        ]
+      },
     ]
+  },
+  actions: {
+    confirmarIncripcion() {
+      alert('Inscripcion confirmada');
+      $('#materias').hide();
+    }
   }
 });
